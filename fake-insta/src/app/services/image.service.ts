2250842b283dabd195a471909638f7a1b6a46720
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2022-09-18 13:43:25 -0300

    Post image and service

---
+++ fake-insta/src/app/services/image.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { catchError, Observable, of, tap } from 'rxjs';
+
+import { Image } from '../models/image';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ImageService {
+  private imageUrl = 'api/post';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) { }
+
+  // GET image from mock DB
+  getImage(postImage: string): Observable<Image> {
+    const url = `${this.imageUrl}/${postImage}`;
+    return this.http.get<Image>(url).pipe(
+      tap((_) => console.log(`fetched image with image=${postImage}`)),
+      catchError(this.handleError<Image>(`getImage image=${postImage}`))
+    );
+  }
+
+  // GET images from mock DB
+  getImages(): Observable<Image[]> {
+    return this.http.get<Image[]>(this.imageUrl).pipe(
+      tap((_) => console.log('fetched images')),
+      catchError(this.handleError<Image[]>('getImages', []))
+    );
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+  // TODO: implement better logging mechanism
+  log(message: string) {
+    console.log(`ImageService: ${message}`);
+  }
+
+}
--- fake-insta/src/app/services/in-memory-data.service.ts
+++ fake-insta/src/app/services/in-memory-data.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
 import { Observable, of } from 'rxjs';
+
+import { Image } from '../models/image';
 import { User } from '../models/user'; 
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class InMemoryDataService extends InMemoryDbService {
   createDb()  {
     const users: User[] = [
@@ -14,9 +18,18 @@ export class InMemoryDataService extends InMemoryDbService {
       { firstName: 'Dulas', lastName: 'Sarabia', username: '@dsarabia', profilePic: '../../assets/profile-pics/toa-heftiba-O3ymvT7Wf9U-unsplash.jpg' },
       { firstName: 'Reynaldo', lastName: 'Rincón', username: '@reyrincon', profilePic: '../../assets/profile-pics/podpros-UpiF461EAHU-unsplash.jpg' }
     ];
-    return {users};
 
+    const images: Image[] = [
+      { user: users[0], postImage: '../../assets/posts/alan-king-KZv7w34tluA-unsplash.jpg'},
+      { user: users[0], postImage: '../../assets/posts/alvan-nee-9M0tSjb-cpA-unsplash.jpg'},
+      { user: users[1], postImage: '../../assets/posts/brian-mcgowan-P1-6ioOcGNU-unsplash (1).jpg'},
+      { user: users[1], postImage: '../../assets/posts/jaime-dantas-Fpvr7thkAf0-unsplash.jpg'},
+      { user: users[2], postImage: '../../assets/posts/ian-dooley-TLD6iCOlyb0-unsplash.jpg'},
+      { user: users[2], postImage: '../../assets/posts/kelsey-curtis-dIs-MqalSSE-unsplash.jpg'},
+      { user: users[3], postImage: '../../assets/posts/keszthelyi-timi-ezUDjYy17EA-unsplash.jpg'},
+      { user: users[3], postImage: '../../assets/posts/lokesh-paduchuri-xOBGLCETJCo-unsplash.jpg'}
+    ];
+
+    return {users, images};
   }
-
-  
 }
