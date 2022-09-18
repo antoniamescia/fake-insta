07b73a8358762852ba7dc7bc8b6303a966c654c5
antoniamescia <antonia.mescia.novarese@gmail.com>
2022-09-18 16:40:47 -0300

    First approach to navbar

--- fake-insta/src/app/app.module.ts
+++ fake-insta/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { InstaButtonComponent } from './insta-button/insta-button.component';
+import { BottomNavbarComponent } from './bottom-navbar/bottom-navbar.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    InstaButtonComponent,    
+    InstaButtonComponent,
+    BottomNavbarComponent,    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
